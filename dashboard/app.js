2fd03cb8b55551373529264846df10ea29afbde3
slam27893-del <slam27893@gmail.com>
2026-07-01 05:31:24 +0300

    Update app.js

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,1 +1,9 @@
+const express = require("express");
 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>🚀 Boredom Dashboard</h1><p>Dashboard is running successfully!</p>");
+});
+
+module.exports = app;
