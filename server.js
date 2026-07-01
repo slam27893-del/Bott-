dac392b80888e41e91abb2c591e715ca5c619131
slam27893-del <slam27893@gmail.com>
2026-07-01 19:45:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,23 @@ app.set("views", path.join(__dirname, "views"));
 app.get("/", (req, res) => {
   const guilds = db.prepare("SELECT * FROM guild_settings").all();
 
-  res.send(`
-    <h1>🎉 لوحة التحكم</h1>
-    <p>عدد السيرفرات المسجلة: ${guilds.length}</p>
-  `);
+  let html = `
+    <h1>🎉 لوحة تحكم Boredom Bot</h1>
+    <p>عدد السيرفرات: ${guilds.length}</p>
+    <hr>
+    <h2>السيرفرات</h2>
+    <ul>
+  `;
+
+  guilds.forEach(guild => {
+    html += `<li>🖥️ ${guild.guild_id}</li>`;
+  });
+
+  html += `
+    </ul>
+  `;
+
+  res.send(html);
 });
 
 const PORT = process.env.PORT || 3000;
