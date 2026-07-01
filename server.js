f9a2940d9a7f942192b0f0ebab800a475dfd0871
slam27893-del <slam27893@gmail.com>
2026-07-01 07:01:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+const express = require("express");
 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("لوحة التحكم تعمل ✅");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Dashboard running on port ${PORT}`);
+});
