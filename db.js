5928ca8a6d6df642dfe429999f7154c6c6e5e770
slam27893-del <slam27893@gmail.com>
2026-07-01 07:17:53 +0300

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@
 const Database = require("better-sqlite3");
 const path = require("path");
 
-const DB_PATH = path.join(__dirname, "..", "levels.db");
+const DB_PATH = path.join(__dirname, "levels.db");
 const db = new Database(DB_PATH);
 
 db.pragma("journal_mode = WAL");
