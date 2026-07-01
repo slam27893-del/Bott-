2289f0c568bd66286ba62753624300747de212b9
slam27893-del <slam27893@gmail.com>
2026-07-01 07:28:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const path = require("path");
 const db = require("./db");
 
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   const guilds = db.prepare("SELECT * FROM guild_settings").all();
